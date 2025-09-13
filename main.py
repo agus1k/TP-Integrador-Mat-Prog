cf78c5248d5bd81bbb7d151b2cbae280f29b48f2
Agustin Miranda <agusmiranda1611@gmail.com>
2025-09-13 13:17:47 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# Juego de Adivinanza en Binario:
+# Muestren un número en binario y desafíen al usuario a adivinar su equivalente decimal, o viceversa, reforzando la conversión entre ambos sistemas.
+
+print("Bienvenido al juego para adivinar un numero binario o decimal!")
+print("""
+    Elija su opción:
+    1. Adivinar número binario
+    2. Adivinar número decimal
+    3. Salir
+""")
+
+# binario = ""
+
+# numero_decimal = int(input("Ingrese un número decimal: "))
+
+# if numero_decimal == 0:
+#     binario = "0"
+# else:
+#     while numero_decimal > 0:
+#         residuo = numero_decimal % 2
+#         binario = str(residuo) + binario
+#         numero_decimal = numero_decimal // 2
+
+# print("El número en binario es:", binario)
